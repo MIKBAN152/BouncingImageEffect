e8ed78a65fe4f0cc58daea7044195a2bc4a386f4
Mikban152 <mikban152@gmail.com>
2022-06-11 03:49:32 -0500

    with sample

--- app/src/main/java/com/example/bouncingeffect/MainActivity.java
+++ app/src/main/java/com/example/bouncingeffect/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.bouncingeffect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //objects
+    ImageView im_big_circle, im_small_circle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        im_big_circle = (ImageView) findViewById(R.id.IV_big_circle);
+        im_small_circle = (ImageView) findViewById(R.id.IV_small_circle);
+
+
+        //BouncingEffect be = new BouncingEffect(im_big_circle,im_small_circle,5,bottomDif,periodUp,periodDown,true);
+        BouncingEffect be = new BouncingEffect(im_big_circle,im_small_circle,4);
+
+        im_small_circle.setOnClickListener(view -> {
+            be.StartBounce();
+        });
+        im_big_circle.setOnClickListener(view -> {
+            be.ResetPosition();
+        });
 
     }
 }
